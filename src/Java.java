0c34c20618cb49e2d766fc4c053fe77e9a88cb76
nezuuuuuuu <146615433+nezuuuuuuu@users.noreply.github.com>
2023-12-14 23:05:10 +0800

    Add files via upload

---
+++ src/Java.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author janed
+ */
+public class Java extends CodeEditor{
+      Java(String passedPath) {
+            super(passedPath, "java");
+        }
+}
---
+++ src/Python.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author janed
+ */
+public class Python extends CodeEditor{
+       Python(String passedPath) {
+            super(passedPath, "java");
+        }
+}
